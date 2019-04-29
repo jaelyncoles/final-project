f7ae95b445ed6efa16f19037ab9c1c009791c03f
jaelyn coles <jaelyncoles@Matthews-MBP-2.localdomain>
2019-04-29 13:44:36 -0400

    first commit

---
+++ database/migrations/2019_04_29_161051_create_venue_post_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVenuePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('venue_post', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('description');
+            $table->string('title');
+            $table->string('photo');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('venue_post');
+    }
+}
