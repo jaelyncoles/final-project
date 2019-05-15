5421d884463c15f871341acddb30820f0cae2fa1
jaelyn coles <jaelyncoles@Matthews-MBP-2.localdomain>
2019-05-15 09:37:11 -0400

    hard coded appname

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'WhatUp!') }}</title>
+    <title>WhatUp!</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
